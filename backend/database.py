b5aeb90cfecdfd095af9210db0f242f83b726033
all-coder <niranjanmagesh@gmail.com>
2025-06-17 18:15:27 +0530

    created docker files

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
-# PostgreSQL Database URL
-URL_DATABASE='postgresql://postgres:postgre@localhost:5432/Neurix'
+
+load_dotenv()
+URL_DATABASE=os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
--- frontend/src/util/helper.js
+++ frontend/src/util/helper.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export async function apiGet(endpoint) {
   const res = await fetch(`${BASE_URL}${endpoint}`, {
